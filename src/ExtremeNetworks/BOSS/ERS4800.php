41720fc0fac054b2d980351e1373b7420096e427
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2023-02-12 19:02:09 +0100

    Update Node Extreme Networks #patch

--- src/ExtremeNetworks/BOSS/ERS4800.php
+++ src/ExtremeNetworks/BOSS/ERS4800.php
@@ -8,6 +8,9 @@
         private $configReport;
         private $runningPortInterface = false;
         private $runningPortInterfaceName = null;
+        private $runningSystemInfo = false;
+        private $runningSystemUnit = null;
+        private $runningSystemUptime = false;
 
         public function __construct() {
             parent::__construct();
@@ -16,7 +19,8 @@
         public function analyseConfigFile($configFile, $configReport){
             $this->configReport = parent::analyseConfigFile($configFile, $configReport);
             foreach ($configFile as $k => $v){
-                $this->analyseRunningInterfaces($k, $v);
+                //$this->analyseRunningInterfaces($k, $v);
+                $this->analyseRunningSystem($k, $v);
             }
 
             $configReport = $this->configReport;
@@ -27,12 +31,10 @@
         private function analyseRunningInterfaces($key, $line){
             if(preg_match("#^\*\*\*\*\*Port Interface\*\*\*\*\*#", $line, $match)){
                 $this->runningPortInterface = true;
-
                 $this->configReport[$key] = true;
-            } elseif(preg_match("#^Unit 1$#", $line)){
+            } elseif($this->runningPortInterface && preg_match("#^Unit 1$#", $line)){
                 $this->runningPortInterface = false;
                 $this->runningPortInterfaceName = null;
-
                 $this->configReport[$key] = true;
             } elseif($this->runningPortInterface && preg_match("#^Unit\/Port:  ([0-9]+)\/([0-9]+)#", $line, $match)){
                 $this->runningPortInterfaceName = [
@@ -40,7 +42,6 @@
                     'pic'   => 0,
                     'port'  => $match[2]
                 ];
-
                 $this->configReport[$key] = true;
             } elseif($this->runningPortInterface && !is_null($this->runningPortInterfaceName)){
                 if(preg_match("#^    Admin Status:  (.*)#", $line, $match)){
@@ -60,6 +61,69 @@
                         $this->getOpenRunning()->getInterfaces()->addEthernet($this->runningPortInterfaceName['fpc'], $this->runningPortInterfaceName['pic'], $this->runningPortInterfaceName['port'])->setOperStatus(false);
                     }
 
+                    $this->configReport[$key] = true;
+                }
+            }
+        }
+
+        private function analyseRunningSystem($key, $line){
+            if(preg_match("#^\*\*\*\*\*SYS Info\*\*\*\*\*#", $line, $match)){
+                $this->runningSystemInfo = true;
+                $this->configReport[$key] = true;
+            } elseif($this->runningSystemInfo && preg_match("#^Unit \#([0-9]+)#", $line, $match)){
+                $this->getOpenRunning()->getSystem()->addStackUnit($match[1]);
+                $this->runningSystemUnit = $match[1];
+                $this->configReport[$key] = true;
+            } elseif($this->runningSystemInfo && preg_match('#^Unit\# Switch Model     Unit UpTime#', $line)){
+                $this->runningSystemUnit = null;
+                $this->runningSystemUptime = true;
+                $this->configReport[$key] = true;
+            } elseif($this->runningSystemUnit && $this->runningSystemUnit){
+                if(preg_match("#^Serial Number:        (.*)#", $line, $match)){
+                    $this->getOpenRunning()->getSystem()->addStackUnit($this->runningSystemUnit)->setSerialNumber($match[1]);
+                    $this->configReport[$key] = true;
+                } elseif(preg_match("#^Power Status:         (.*)#", $line, $match)){
+                    $this->getOpenRunning()->getSystem()->addStackUnit($this->runningSystemUnit)->setPowerStatus($match[1]);
+                    $this->configReport[$key] = true;
+                } elseif(preg_match("#^Switch Model:         (.*)#", $line, $match)){
+                    $this->getOpenRunning()->getSystem()->addStackUnit($this->runningSystemUnit)->setSwitchModel($match[1]);
+                    $this->configReport[$key] = true;
+                } elseif(preg_match("#^Firmware Version:     (.*)#", $line, $match)){
+                    $this->getOpenRunning()->getSystem()->addStackUnit($this->runningSystemUnit)->setVersionFirmware($match[1]);
+                    $this->configReport[$key] = true;
+                } elseif(preg_match("#^Software Version:     (.*)#", $line, $match)){
+                    $this->getOpenRunning()->getSystem()->addStackUnit($this->runningSystemUnit)->setVersionSoftware($match[1]);
+                    $this->configReport[$key] = true;
+                } elseif(preg_match("#^Fan \#([0-9]) Status:        (.*)#", $line, $match)){
+                    $this->getOpenRunning()->getSystem()->addStackUnit($this->runningSystemUnit)->addFanStatus($match[1], $match[2]);
+                    $this->configReport[$key] = true;
+                }
+            } elseif($this->runningSystemInfo && $this->runningSystemUptime) {
+                if(preg_match("#^$#", $line, $match)){
+                    $this->runningSystemUptime = false;
+                    $this->configReport[$key] = true;
+                } elseif(preg_match("#^([0-9])     (.*)          (.*)#", $line, $match)){
+                    $this->getOpenRunning()->getSystem()->addStackUnit($match[1])->setSwitchUptime($match[3]);
+                    $this->configReport[$key] = true;
+                }
+            } elseif($this->runningSystemInfo) {
+                if(preg_match("#^\*\*\*\*\*MEMORY INFORMATION\*\*\*\*\*#", $line)){
+                    $this->runningSystemInfo = false;
+                    $this->configReport[$key] = true;
+                } elseif(preg_match("#^Size Of Stack:        ([0-9]+)#", $line, $match)){
+                    $this->getOpenRunning()->getSystem()->setSizeStack($match[1]);
+                    $this->configReport[$key] = true;
+                } elseif(preg_match("#^Base Unit:            ([0-9]+)#", $line, $match)){
+                    $this->getOpenRunning()->getSystem()->setBaseUnitStack($match[1]);
+                    $this->configReport[$key] = true;
+                } elseif(preg_match("#^sysName:              (.*)#", $line, $match)){
+                    $this->getOpenRunning()->getSystem()->setSysName($match[1]);
+                    $this->configReport[$key] = true;
+                } elseif(preg_match("#^Installed license:     (.*)#", $line, $match)){
+                    $this->getOpenRunning()->getSystem()->addInstalledLicence($match[1]);
+                    $this->configReport[$key] = true;
+                } elseif(preg_match("#^sysUpTime:            (.*)#", $line, $match)){
+                    $this->getOpenRunning()->getSystem()->setSwitchUptime($match[1]);
                     $this->configReport[$key] = true;
                 }
             }
